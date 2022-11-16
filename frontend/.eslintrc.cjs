41fd70fdb81308607bb12b2f1fe5df03d241dd63
Pierre <jpcampoverde9801@hotmail.com>
2022-11-15 23:35:53 -0300

    Added all necessary libraries for dev and testing

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,22 +2,25 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
   },
   extends: [
     'airbnb',
     'airbnb/hooks',
+    'airbnb-typescript',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/react-in-jsx-scope': 0,
   },
 };
