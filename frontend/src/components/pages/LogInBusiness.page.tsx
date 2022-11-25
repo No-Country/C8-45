601824480795f125d87cef1dfb12b41b9afb1be1
rony171998 <rony171998@gmail.com>
2022-11-25 14:59:43 -0500

    create loginBusinessPage

---
+++ frontend/src/components/pages/LogInBusiness.page.tsx
@@ -0,0 +1,21 @@
+import LogInBusinessForm from '../organisms/LogInBusinessForm';
+
+function LogInBusiness() {
+  return (
+    <section className="w-full flex-col h-[84vh]  flex md:flex-row items-center justify-center my-auto">
+      <div className="w-full md:w-6/12">
+        <div className="w-10/12 sm:w-7/12 md:w-8/12 lg:w-6/12 mx-auto">
+          <LogInBusinessForm />
+        </div>
+      </div>
+      <div className="hidden md:block md:w-1/4">
+        <img
+          className="rounded-r-3xl"
+          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default LogInBusiness;
