a1fde0dbec4e6e83cb61c1d3953225c5aa5a5daf
Pierre <jpcampoverde9801@hotmail.com>
2022-11-25 01:27:48 -0300

    User stats section made responsive

--- frontend/src/components/organisms/UserStats.tsx
+++ frontend/src/components/organisms/UserStats.tsx
@@ -7,7 +7,7 @@ function UserStats() {
   const { user } = useAppSelector((state) => state.auth);
   //TODO make a fetch to each user
   return (
-    <div className="flex items-center justify-around bg-indigo-50 p-10 lg:w-9/12 mx-auto rounded-2xl">
+    <div className="flex-col flex md:flex-row items-center justify-around bg-indigo-50 p-10 lg:w-9/12 mx-auto rounded-2xl">
       <UserStatsAvatar />
       <div className="flex flex-col items-center">
         <div className="flex text-blue-600 items-center  font-poppins font-bold">
