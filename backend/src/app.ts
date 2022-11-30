3dba416c2bbf8b294d2283b95004fc9587b9554c
Alvaro1599 <alvaro1599@gmail.com>
2022-11-30 06:03:38 -0500

    swagger docs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,17 +2,18 @@ import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import path from "path";
-
+import swaggerUi from "swagger-ui-express"
 import router from "./routes";
+import swaggerJson from "./swagger.json"
 //create server
 const app = express();
 //configuration
 app.set("port", process.env.PORT || 3000);
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "../public")));
+app.use("/docs",swaggerUi.serve,swaggerUi.setup(swaggerJson))
 app.use(router);
 export default app;
