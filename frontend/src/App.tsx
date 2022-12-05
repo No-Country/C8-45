dcafdf65a32115ca715c647950a4f3f123cf7697
Pierre <jpcampoverde9801@hotmail.com>
2022-12-05 17:18:09 -0300

    Roles and layout fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,33 +21,53 @@ import NoFound from './components/organisms/NoFound';
 
 function App() {
   const { user } = useAppSelector((state) => state.auth);
-  const isCompany = user?.role.id === 3
-  const isAdmin = user?.role.id === 1
+  const isCompany = user?.role.id === 3;
+  const isAdmin = user?.role.id === 2;
 
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route index element={<Landing />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="login-business" element={<LogInBusiness />} />
-        <Route path="signup-business" element={<SignupBusiness />} />
-        <Route path="createReview" element={<CreateReview />} />
-        <Route path="how-it-works" element={<Howitworks />} />
-        <Route path="for-business" element={<ForBusiness />} />
-        <Route path="business/:id" element={<BusinessProfile />} />
-        <Route path="search/:search" element={<SearchPage />} />
-        <Route path="/me" element={isAdmin ? <BusinessProfile /> : <MyProfile />}>
-          <Route index element={isAdmin ? '' : <ReviewFormPage />} />
-          <Route path="reviews" element={isAdmin ? <MyBusinessReviews /> : <MyReviews />} />
-          <Route path="settings" element={isAdmin ? <BusinessSettings /> : <UserSettings />} />
-          <Route path="/me" element={isCompany ? <BusinessProfile /> : <MyProfile />}>
-            <Route index element={isCompany ? '' : <ReviewFormPage />} />
-            <Route path="reviews" element={isCompany ? <MyBusinessReviews /> : <MyReviews />} />
-            <Route path="settings" element={isCompany ? <BusinessSettings /> : <UserSettings />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login-business" element={<LogInBusiness />} />
+          <Route path="signup-business" element={<SignupBusiness />} />
+          <Route path="createReview" element={<CreateReview />} />
+          <Route path="how-it-works" element={<Howitworks />} />
+          <Route path="for-business" element={<ForBusiness />} />
+          <Route path="business/:id" element={<BusinessProfile />} />
+          <Route path="search/:search" element={<SearchPage />} />
+          <Route
+            path="/me"
+            element={isAdmin ? <BusinessProfile /> : <MyProfile />}
+          >
+            <Route index element={isAdmin ? '' : <ReviewFormPage />} />
+            <Route
+              path="reviews"
+              element={isAdmin ? <MyBusinessReviews /> : <MyReviews />}
+            />
+            <Route
+              path="settings"
+              element={isAdmin ? <BusinessSettings /> : <UserSettings />}
+            />
+            {/* Todo Revisar este rol? */}
+            {/* <Route
+              path="/me"
+              element={isCompany ? <BusinessProfile /> : <MyProfile />}
+            >
+              <Route index element={isCompany ? '' : <ReviewFormPage />} />
+              <Route
+                path="reviews"
+                element={isCompany ? <MyBusinessReviews /> : <MyReviews />}
+              />
+              <Route
+                path="settings"
+                element={isCompany ? <BusinessSettings /> : <UserSettings />}
+              />
+            </Route> */}
+            <Route path="*" element={<NoFound />} />
           </Route>
-          <Route path="*" element={<NoFound />} />
         </Route>
       </Routes>
     </div>
