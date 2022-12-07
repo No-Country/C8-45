02f22e7ad3d62456c695aad3f359cd856d07007d
MorardRamiro <rramiromorard@gmail.com>
2022-12-07 18:26:12 -0300

    fixed some errors

--- frontend/src/components/pages/Login.page.tsx
+++ frontend/src/components/pages/Login.page.tsx
@@ -5,19 +5,19 @@ import { useAppSelector } from '../../redux/store';
 import LogInForm from '../organisms/LogInForm';
 
 function Login() {
-  const {user,token}=useAppSelector(state=>state.auth);
-  const navigate=useNavigate();
-  const location=useLocation();
+  const { user, token } = useAppSelector(state => state.auth);
+  const navigate = useNavigate();
+  const location = useLocation();
   useEffect(() => {
-    if(user&&token){
+    if (user && token) {
       navigate(`/me`)
     }
-  }, [user,token])
+  }, [user, token])
   return (
     <section className="min-h-[86vh] flex">
       <div className=" w-full my-auto my-24 ">
         <h2 className="text-center text-3xl font-title my-4">
-          Lorem ipsum dolor sit amet.
+          Log in whit your email.
         </h2>
         <div className="w-11/12 sm:w-8/12 md:w-5/12 lg:w-3/12 mx-auto my-auto">
           <LogInForm />
